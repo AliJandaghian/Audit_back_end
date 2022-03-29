b7b815e732c3a0f15e3b9d87c2776e99150ad934
Ali Jandaghian <mehrad90aj@yahoo.com>
2022-03-29 14:57:53 -0700

    Add joi for user model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
-const config = require("config")
+const config = require("config");
 const Joi = require("joi");
+require("joi-objectid")(Joi);
+const passwordComplexity = require("joi-password-complexity");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -44,13 +46,39 @@ const userSchema = new mongoose.Schema({
 const User = new mongoose.model("user", userSchema);
 
 userSchema.methods.generateAuthToken = function () {
-  return jwt.sign({
-    _id: this._id,
-    email: this.email,
-    isAuditor: this.isAuditor,
-    isManager: this.isManager,
-    isAdmin: this.isAdmin,
-  },
-  config.get('jetPrivateKey')
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      isAuditor: this.isAuditor,
+      isManager: this.isManager,
+      isAdmin: this.isAdmin,
+    },
+    config.get("jetPrivateKey")
   );
 };
+
+function validateUser(user) {
+  const complexityOptions = {
+    min: 6,
+    max: 50,
+    lowerCase: 1,
+    upperCase: 1,
+    numeric: 1,
+    symbol: 1,
+    requirementCount: 2,
+  };
+
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(50).required(),
+    email: Joi.string().email().required(),
+    departmentId: Joi.objectId().required(),
+    password: passwordComplexity(complexityOptions),
+  });
+
+  return schema.validate(user);
+}
+
+module.exports.validateUser = validateUser;
+module.exports.User = User;
+module.exports.userSchema = userSchema;
