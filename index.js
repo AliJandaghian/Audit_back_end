25e74ad183e1ff2accf5f815cd7021bc0d60d9b2
Ali Jandaghian <mehrad90aj@yahoo.com>
2022-04-21 12:58:18 -0700

    Add joi validation module to startup

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
-
-const debug = require("debug")("app:startup");
-
+require('./startup/validation')()
+const logger = require("./startup/logging");
 const express = require("express");
 const app = express();
+
+logger
 require('./startup/prod')(app)
-require("./startup/logging");
 require('./startup/routes')(app)
 require("./startup/db")();
 require('./startup/config')()
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => debug(`Listeting to port ${port} ...`));
+app.listen(port, () => logger.log({level: 'info', message:`Listeting to port ${port} ...`}));
