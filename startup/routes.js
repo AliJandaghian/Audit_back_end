9e20bfa2d0f6055b04999b15347948465600c0f8
Ali Jandaghian <mehrad90aj@yahoo.com>
2022-04-21 10:34:14 -0700

    Add routes to startup

---
+++ startup/routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const error = require("../middleware/error");
+const defects = require("../routes/defects");
+const machines = require("../routes/machines");
+const audits = require("../routes/audits");
+const users = require("../routes/users");
+const auditSettings = require("../routes/auditSettings");
+const auth = require("../routes/auth");
+const departments = require("../routes/departments");
+
+module.exports = function (app) {
+  app.use(express.json());
+  app.use("/api/auth", auth);
+  app.use("/api/defects", defects);
+  app.use("/api/users", users);
+  app.use("/api/auditsettings", auditSettings);
+  app.use("/api/departments", departments);
+  app.use("/api/machines", machines);
+  app.use("/api/audits", audits);
+  app.use(error);
+};
