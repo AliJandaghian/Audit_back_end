bce8fad181e08ad346169306c994fb14baa595db
Ali Jandaghian <mehrad90aj@yahoo.com>
2022-04-21 12:29:56 -0700

    Add prod module with compression and helemt pakages

--- index.js
+++ index.js
@@ -3,20 +3,13 @@ const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 
 const debug = require("debug")("app:startup");
-const helmet = require("helmet");
 
 const express = require("express");
-
 const app = express();
-
-
+require('./startup/prod')(app)
 require("./startup/logging");
-
-app.use(helmet());
-
 require('./startup/routes')(app)
 require("./startup/db")();
-
 require('./startup/config')()
 
 const port = process.env.PORT || 3000;
